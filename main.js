8b6b56ec53195c2ffe67f028965bc8de69fff9d5
Henrik Næss <henrik@heka.no>
2022-04-28 09:59:02 +0200

    Refactored to use classes

--- main.js
+++ main.js
@@ -1,88 +1,198 @@
-let dragggingWindowEl = null;
-
-let dragOffset = { x: null, y: null };
+let windows = [];
+let drag = null;
 
 const defaultWidth = 800;
 const defaultHeight = 600;
 
-function addWindow() {
-	// Make the header element
-	const windowHeaderEl = document.createElement('div');
-	windowHeaderEl.classList.add('window-header');
+class UiElement {
+	constructor(name, type, classes) {
+		this.name = name;
 
-	// Make the exit button element
-	const windowHeaderExitEl = document.createElement('button');
-	windowHeaderExitEl.classList.add('window-header-exit')
-	windowHeaderExitEl.innerHTML = 'X';
+		this.el = document.createElement(type);
+		this.el.classList.add(classes);
 
-	// Put the exit button inside the header
-	windowHeaderEl.appendChild(windowHeaderExitEl);
+		this.children = [];
+		this.parent = null;
+	}
 
-	// Make the body element
-	const windowBodyEl = document.createElement('div');
-	windowBodyEl.classList.add('window-body');
+	appendChild(uiElement) {
+		this.el.appendChild(uiElement.el);
+		this.children.push(uiElement);
+		uiElement.setParent(this);
+	}
 
-	// Make the window container.
-	const windowEl = document.createElement('div');
+	setParent(uiElement) {
+		this.parent = uiElement;
+	}
 
-	// Put the header and the body inside the window container.
-	windowEl.appendChild(windowHeaderEl);
-	windowEl.appendChild(windowBodyEl);
+	getChild(name) {
+		return this.children.find(uiElement => uiElement.name === name);
+	}
 
-	windowEl.classList.add('window');
-	windowEl.style.width = `${defaultWidth}px`;
-	windowEl.style.height = `${defaultHeight}px`;
+	removeChild(child) {
+		let index = null;
+		if (typeof (child) === 'string') {
+			index = this.children.findIndex(uiElement => uiElement.name === child);
+		} else {
+			index = this.children.findIndex(uiElement => uiElement === child);
+		}
 
-	// Remove the window when you press the exit button
-	windowHeaderExitEl.addEventListener('click', () => {
-		document.body.removeChild(windowEl);
-	});
-
-	// start dragging
-	windowHeaderEl.addEventListener('mousedown', event => {
-		event.preventDefault();
-
-		// Add the dragging class
-		windowEl.classList.add('dragging');
-
-		// Set the current dragging window
-		dragggingWindowEl = windowEl;
-
-		// Set the offset where the drag started (relative to the window container)
-		dragOffset.x = windowEl.offsetLeft - event.clientX;
-		dragOffset.y = windowEl.offsetTop - event.clientY;
-	});
-
-	windowEl.addEventListener('mousedown', event => {
-		event.preventDefault();
-
-		// Put the last clicked at the top (and all others at the bottom).
-		document.querySelectorAll('.window').forEach(el => el.style.zIndex = 10);
-		windowEl.style.zIndex = 20;
-	})
-
-	// Add the window to the document body.
-	document.body.appendChild(windowEl);
+		if (index !== null) {
+			this.el.removeChild(this.children[index].el);
+			return this.children.splice(index, 1);
+		}
+	}
 }
 
-// Move the current dragging window
+class WindowHeaderExitButton extends UiElement {
+	constructor() {
+		super('exit-button', 'button', 'window-header-exit');
+
+		this.el.innerHTML = 'X';
+
+		this.el.addEventListener('click', event => {
+			event.preventDefault()
+			this.parent.parent.parent.removeWindow(this.parent.parent);
+		});
+	}
+}
+
+class WindowHeaderTitle extends UiElement {
+	constructor() {
+		super('title', 'span', 'window-header-title');
+	}
+
+	setTitle(title) {
+		this.el.innerText = title
+	}
+}
+
+class WindowHeader extends UiElement {
+	constructor() {
+		super('header', 'div', 'window-header');
+
+		this.appendChild(new WindowHeaderTitle());
+		this.appendChild(new WindowHeaderExitButton());
+
+		this.el.addEventListener('mousedown', event => {
+			event.preventDefault();
+
+			this.parent.el.classList.add('dragging');
+
+			drag = {
+				offset: {
+					x: this.parent.el.offsetLeft - event.clientX,
+					y: this.parent.el.offsetTop - event.clientY
+				},
+				window: this.parent
+			}
+		});
+	}
+
+	setTitle(title) {
+		this.getChild('title').setTitle(title)
+	}
+}
+
+class WindowBody extends UiElement {
+	constructor() {
+		super('body', 'div', 'window-body');
+	}
+}
+
+class Window extends UiElement {
+	constructor(title) {
+		super('window', 'div', 'window');
+
+		this.el.style.width = `${defaultWidth}px`;
+		this.el.style.height = `${defaultHeight}px`;
+
+		this.appendChild(new WindowHeader());
+		this.appendChild(new WindowBody());
+
+		this.setTitle(title);
+
+		this.el.addEventListener('mousedown', event => {
+			this.parent.setFocus(this);
+		})
+	}
+
+	setTitle(title) {
+		this.getChild('header').setTitle(title);
+	}
+
+	setZIndex(zIndex) {
+		this.el.style.zIndex = zIndex;
+	}
+}
+
+class Display extends UiElement {
+	constructor() {
+		super('display', 'div', 'display');
+		this.focus = [];
+	}
+
+	addWindow() {
+		let window = new Window();
+		let index = this.children.length + 1;
+		window.setTitle(`Window ${index}`)
+		this.appendChild(window)
+	}
+
+	removeWindow(child) {
+		let childIndex = this.children.findIndex(el => el == child);
+		let focusIndex = this.focus.findIndex(i => i === childIndex);
+
+		this.focus.splice(focusIndex, 1);
+		this.removeChild(child);
+	}
+
+	updateZIndexes() {
+		for (let focusIndex in this.focus) {
+			console.log(focusIndex, 10);
+			console.log(focusIndex + 10)
+			this.children[this.focus[focusIndex]].setZIndex(focusIndex + 10);
+		}
+	}
+
+	setFocus(child) {
+		let childIndex = this.children.findIndex(el => el == child);
+
+		if (childIndex === -1)
+			return;
+
+		let focusIndex = this.focus.findIndex(i => i === childIndex);
+
+		if (focusIndex !== -1)
+			this.focus.splice(focusIndex, 1)
+		this.focus.push(childIndex);
+
+		this.updateZIndexes();
+	}
+}
+
+const display = new Display();
+document.body.appendChild(display.el);
+
+function addWindow() {
+	display.addWindow();
+}
+
 document.addEventListener('mousemove', event => {
-	if (dragggingWindowEl) {
+	if (drag) {
 		event.preventDefault();
 		// set the position
-		dragggingWindowEl.style.top = (dragOffset.y + event.clientY) + "px";
-		dragggingWindowEl.style.left = (dragOffset.x + event.clientX) + "px";
+		drag.window.el.style.top = (drag.offset.y + event.clientY) + "px";
+		drag.window.el.style.left = (drag.offset.x + event.clientX) + "px";
 	}
 });
 
-// stop dragging
 document.addEventListener('mouseup', event => {
-	if (dragggingWindowEl) {
+	if (drag) {
 		event.preventDefault();
-		dragggingWindowEl.classList.remove('dragging');
-		dragggingWindowEl = null;
+		drag.window.el.classList.remove('dragging');
+		drag = null;
 	}
 })
 
-// make new window when the button is pressed
 document.getElementById("add-new").addEventListener('click', () => addWindow());
